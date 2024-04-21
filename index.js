54d1db01bfa1d38152528b2b5f641c6f1bc2405d
shah7879 <160831851+shah7879@users.noreply.github.com>
2024-04-21 16:48:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from 'inquirer';
+const currency = {
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "enter from currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "to",
+        message: "enter from currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: "enter your amount",
+        type: 'number',
+    }
+]);
+let fromamount = currency[user_answer.from];
+let toamount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseamount = amount / fromamount; //usd base currency
+let convertedamount = baseamount * toamount;
+console.log(convertedamount);
+console.log(fromamount);
+console.log(toamount);
+console.log(amount);
